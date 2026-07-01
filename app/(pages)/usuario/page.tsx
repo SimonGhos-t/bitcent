055f59680f77a558bfd2e533f734eed9992259b0
Robson <robsonvale135@gmail.com>
2026-06-30 20:52:08 -0400

    versão estável 1.1

--- app/(pages)/usuario/page.tsx
+++ app/(pages)/usuario/page.tsx
@@ -1,13 +1,19 @@
 import Conteudo from "@/app/src/components/template/Conteudo";
 import Cabecalho from "../../src/components/template/Cabecalho";
 import Pagina from "../../src/components/template/Pagina";
+import TituloPagina from "@/app/src/components/template/TituloPagina";
+import { IconForms } from "@tabler/icons-react";
+import usuario from "@/app/src/data/constants/usuarioFalso";
+import Formularios from "@/app/src/components/usuario/Formularios";
 
 export default function Home() {
+
     return (
         <Pagina>
             <Cabecalho/>
             <Conteudo>
-                <div>Usuario</div>
+                <TituloPagina icone={<IconForms/>} principal="Dados Cadastrais" secundario={`Informações de ${usuario.email}`}/>
+                <Formularios/>
             </Conteudo>
         </Pagina>
     )	
